60f6a845564f44dbc4282080613b875163ae2348
kawahiro <k.hiro.m305@gmail.com>
2012-05-08 15:02:49 +0900

    追加

--- cube.js
+++ cube.js
@@ -5,5 +5,3 @@ var green = [ [1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] ];
 var orenge = [ [1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] ];
 var white = [ [1 , 2 , 3] , [4 , 5 , 6] , [7 , 8 , 9] ];
 
-console.log(red);
-console.log(bule);
